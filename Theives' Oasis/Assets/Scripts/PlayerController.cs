2422b1c156eb50d3140c7cbbb99233ba7f1d6b08
micpap25 <34077605+micpap25@users.noreply.github.com>
2019-10-11 15:44:40 -0400

    Push Halfway Through 10/11/19
    Movement and crouching added

--- Theives' Oasis/Assets/Scripts/PlayerController.cs
+++ Theives' Oasis/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float moveSpeed;
+    public float crouchSpeed;
+    public float currentSpeed;
     void Start()
     {
-        
+        currentSpeed = moveSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed = crouchSpeed;
+        } 
+        else
+        {
+            currentSpeed = moveSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, currentSpeed, 0);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, -currentSpeed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(currentSpeed, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-currentSpeed, 0, 0);
+        }
     }
 }
