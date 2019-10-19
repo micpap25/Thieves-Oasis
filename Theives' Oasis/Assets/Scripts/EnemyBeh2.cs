3a6b9d5682f1112054059c738afe215a3e675875
adruz <adruzenko03@gmail.com>
2019-10-19 12:22:53 -0400

    Roll, collision ideas

---
+++ Theives' Oasis/Assets/Scripts/EnemyBeh2.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float degreeFacing;
+    [SerializeField] private float degreeToPlayer;
+    [SerializeField] private float distanceFromPlayer;
+    [SerializeField] private GameObject player;
+    void Start()
+    {
+        degreeFacing = 0;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        distanceFromPlayer = Mathf.Sqrt(Mathf.Pow(transform.position.x - player.transform.position.x, 2) + Mathf.Pow(transform.position.y - player.transform.position.y, 2));
+        degreeToPlayer = Vector3.SignedAngle(player.transform.position - transform.position, transform.position, Vector3.forward);
+        if (degreeFacing > 160)
+        {
+            if (degreeToPlayer > degreeFacing - 20 && degreeToPlayer < degreeFacing - 340 && distanceFromPlayer < 10)
+            {
+                Debug.Log("ALERT ALERT");
+            }
+        }
+        else if (degreeFacing < -160)
+        {
+            if (degreeToPlayer > degreeFacing + 340 && degreeToPlayer < degreeFacing + 20 && distanceFromPlayer < 10)
+            {
+                Debug.Log("ALERT ALERT");
+            }
+        }
+
+        else
+        {
+            if (degreeToPlayer > degreeFacing - 20 && degreeToPlayer < degreeFacing + 20 && distanceFromPlayer < 10)
+            {
+                Debug.Log("ALERT ALERT");
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            //Destroy(collision.gameObject);
+            Debug.Log("Oof you're dead");
+        }
+    }
+}
--- Theives' Oasis/Assets/Scripts/PlayerController.cs
+++ Theives' Oasis/Assets/Scripts/PlayerController.cs
@@ -8,42 +8,66 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
     public float moveSpeed;
     public float crouchSpeed;
-    public float currentSpeed;
+    public float rollSpeed;
+
+    private float currentSpeed;
+    private bool roll;
+    private int rollStart;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.freezeRotation = true;
         currentSpeed = moveSpeed;
+        roll = false;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.LeftShift))
+        
+
+        if (!roll)
         {
-            currentSpeed = crouchSpeed;
-        } 
-        else
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                currentSpeed = crouchSpeed;
+            }
+
+            else if (Input.GetKeyDown(KeyCode.Tab) && Time.frameCount-rollStart>=60)
+            {
+                currentSpeed = rollSpeed;
+                roll = true;
+                rollStart = Time.frameCount;
+            }
+
+            else
+            {
+                currentSpeed = moveSpeed;
+            }
+        }
+
+        if (roll && Time.frameCount - rollStart >= 10)
         {
             currentSpeed = moveSpeed;
+            roll = false;
         }
 
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(0, moveSpeed, 0);
+            transform.Translate(0, currentSpeed, 0);
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(0, -moveSpeed, 0);
+            transform.Translate(0, -currentSpeed, 0);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(moveSpeed, 0, 0);
+            transform.Translate(currentSpeed, 0, 0);
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(-moveSpeed, 0, 0);
+            transform.Translate(-currentSpeed, 0, 0);
         }
     }
 }
