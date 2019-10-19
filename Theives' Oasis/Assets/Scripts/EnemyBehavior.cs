7a3336fd871b67c3d04e75439ba0e6bd6b15fad1
micpap25 <34077605+micpap25@users.noreply.github.com>
2019-10-19 10:01:09 -0400

    Weekend Push
    Made a working Line of Sight
    Must implement a visualization of it & bugtest more thouroughly about checking angles passing through 180.

--- Theives' Oasis/Assets/Scripts/EnemyBehavior.cs
+++ Theives' Oasis/Assets/Scripts/EnemyBehavior.cs
@@ -5,17 +5,43 @@ using UnityEngine;
 public class EnemyBehavior : MonoBehaviour
 {
     // Start is called before the first frame update
-    public int degreeFacing;
-    public GameObject lineOfSight;
+    public float degreeFacing;
+    [SerializeField] private float degreeToPlayer;
+    [SerializeField] private float distanceFromPlayer;
+    [SerializeField] private GameObject player;
     void Start()
     {
-        
+        degreeFacing = 0;
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
+        distanceFromPlayer = Mathf.Sqrt(Mathf.Pow(transform.position.x - player.transform.position.x, 2) + Mathf.Pow(transform.position.y - player.transform.position.y, 2));
+        degreeToPlayer = Vector3.SignedAngle(player.transform.position - transform.position, transform.position, Vector3.forward);
+        if (degreeFacing > 160)
+        {
+            if(degreeToPlayer > degreeFacing - 20 && degreeToPlayer < degreeFacing - 340 && distanceFromPlayer < 10)
+            {
+                Debug.Log("ALERT ALERT");
+            }
+        }
+        else if (degreeFacing < -160)
+        {
+            if (degreeToPlayer > degreeFacing + 340 && degreeToPlayer < degreeFacing + 20 && distanceFromPlayer < 10)
+            {
+                Debug.Log("ALERT ALERT");
+            }
+        }
+            
+        else
+        {
+            if (degreeToPlayer > degreeFacing - 20 && degreeToPlayer < degreeFacing + 20 && distanceFromPlayer < 10)
+            {
+                Debug.Log("ALERT ALERT");
+            }
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
