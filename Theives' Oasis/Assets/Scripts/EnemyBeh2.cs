a2627acc9e86be1deece901af3d8a436a2656a05
micpap25 <34077605+micpap25@users.noreply.github.com>
2019-10-19 23:49:04 -0400

    Beh2 Fix
    Alex you dummy
    you forgot to refactor Beh2 to be Beh2

--- Theives' Oasis/Assets/Scripts/EnemyBeh2.cs
+++ Theives' Oasis/Assets/Scripts/EnemyBeh2.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBehavior : MonoBehaviour
+public class EnemyBeh2 : MonoBehaviour
 {
     // Start is called before the first frame update
     public float degreeFacing;
